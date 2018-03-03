b15454f3556621c0e72606b25a1fa92529c1cede
John L Papa <john@johnpapa.net>
2018-03-03 23:37:00 +0200

    removed console log of conn string. skip trying to connect to cosmos if process.env.COSMOSDB_DB is missing

--- src/server/services/mongo.js
+++ src/server/services/mongo.js
@@ -12,13 +12,15 @@ const key = encodeURIComponent(process.env.COSMOSDB_KEY);
 const mongoUri = `mongodb://${process.env.COSMOSDB_ACCOUNT}:${key}@${
   process.env.COSMOSDB_ACCOUNT
   }.documents.azure.com:${process.env.COSMOSDB_PORT}/${process.env.COSMOSDB_DB}?ssl=true`;
-console.log(mongoUri);
 // Local MongoDB Connection String
 // const mongoUri = `mongodb://localhost:27017/heroes-db`;
 
 function connect() {
-  mongoose.set('debug', true);
-  return mongoose.connect(mongoUri);
+  if (process.env.COSMOSDB_DB) {
+    mongoose.set('debug', true);
+    return mongoose.connect(mongoUri);
+  }
+  return;
   // return mongoose.connect(mongoUri, { useMongoClient: true });
 }
 
