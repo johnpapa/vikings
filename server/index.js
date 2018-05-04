868f7eb1e85e2d735b956f1da714b3d0c477a28b
John L Papa <john@johnpapa.net>
2018-05-04 16:07:58 -0400

    modified ENV variables for the folder moves

--- server/index.js
+++ server/index.js
@@ -14,17 +14,17 @@ function start() {
   const express = require('express');
   const app = express();
   const port = process.env.PORT || 3001;
-  const publicweb = process.env.PUBLICWEB || './publicweb';
+  const www = process.env.WWW || './';
   const bodyParser = require('body-parser');
   const routes = require('./routes');
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 
-  app.use(express.static(publicweb));
-  console.log(`serving ${publicweb}`);
+  app.use(express.static(www));
+  console.log(`serving ${www}`);
   app.use('/api', routes);
   app.get('*', (req, res) => {
-    res.sendFile(`index.html`, { root: publicweb });
+    res.sendFile(`index.html`, { root: www });
   });
   app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 }
