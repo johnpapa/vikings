fd4f2fb780dbb4af8a8f71f1b18eb87a3140b11e
John L Papa <john@johnpapa.net>
2018-05-31 10:22:24 +0300

    hi everyne!

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import 'hammerjs';
 import { AppDevModule } from './app/app-dev.module';
 import { environment } from './environments/environment';
-
-import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
 }
 
+// If you want to pretend there is a database
 platformBrowserDynamic().bootstrapModule(AppDevModule);
+
+// If you have a real database, use this
 // platformBrowserDynamic().bootstrapModule(AppModule);
