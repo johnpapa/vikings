8137e49af7936fa846541a660fb149a58dabd6ce
John Papa <john@johnpapa.net>
2018-10-05 11:39:56 -0400

    minor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  exports: [
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  declarations: []
-})
-export class SharedModule { }
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [FormsModule, ReactiveFormsModule],
+  declarations: [],
+  })
+export class SharedModule {}
