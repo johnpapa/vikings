7443d6800681148499e18ac6fd3c9a55732ddc86
John Papa <john@johnpapa.net>
2018-10-04 23:09:54 -0400

    stuff

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgrxDataModule } from 'ngrx-data';
 import { environment } from '../../environments/environment';
-import { EntityStoreModule } from './/entity-store.module';
+import { entityConfig } from './entity-metadata';
+import { NgrxDataToastService } from './ngrx-data-toast.service';
 
 @NgModule({
   imports: [
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-    environment.production ? [] : StoreDevtoolsModule.instrument(),
-    EntityStoreModule
-  ]
-})
-export class AppStoreModule {}
+  StoreModule.forRoot({}),
+  EffectsModule.forRoot([]),
+  environment.production ? [] : StoreDevtoolsModule.instrument(),
+  NgrxDataModule.forRoot(entityConfig),
+  ],
+  })
+export class AppStoreModule {
+  constructor(toastService: NgrxDataToastService) {}
+}
