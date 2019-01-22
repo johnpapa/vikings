fe982741aff175b73ad67de68d76e07bd3c27c24
John Papa <john@johnpapa.net>
2019-01-22 10:04:06 -0500

    fixing launch to skip mongo connection if cosmos sdk is chosen

--- server/services/index.js
+++ server/services/index.js
@@ -1,27 +1,27 @@
 const cosmosSDK = require('./cosmos-sdk');
 const mongo = require('./mongo');
-const { connect } = require('./mongo/db');
+const { connect: connectToMongo } = require('./mongo/db');
 
 let useCosmosSDK = false;
 
 const captains = console;
 
-connect();
-
 switch (process.env.DATA_OPTION) {
-  case 'cloud_cosmos':
-    useCosmosSDK = false;
-    captains.log(`===> Using Cosmos with the Mongo API`);
-    break;
-
   case 'cloud_cosmos_sdk':
     useCosmosSDK = true;
     captains.log(`===> Using Cosmos with the Cosmos SDK`);
     break;
 
+  case 'cloud_cosmos':
+    useCosmosSDK = false;
+    captains.log(`===> Using Cosmos with the Mongo API`);
+    connectToMongo();
+    break;
+
   case 'local_mongo':
     useCosmosSDK = false;
     captains.log(`===> Using Mongo`);
+    connectToMongo();
     break;
 
   default:
