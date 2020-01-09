4464f69193c179eb7cf9363982eaf93f38857e0d
John Papa <papa@Johns-MacBook-Pro.local>
2020-01-09 12:10:52 -0500

    upgrade for 8.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['airbnb-base'],
-  plugins: ['html'],
+  // extends: ['airbnb-base'],
+  plugins: ["html"],
   rules: {
-    'no-use-before-define': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-  },
+    "no-use-before-define": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }]
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import "hammerjs";
+import { environment } from "./environments/environment";
+import { AppModule } from "./app/app.module";
+import { AppDevModule } from "./app/app-dev.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
 // If you want to pretend there is a database
-// platformBrowserDynamic().bootstrapModule(AppDevModule);
+platformBrowserDynamic().bootstrapModule(AppDevModule);
 
 // If you have a real database, use this
-platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
