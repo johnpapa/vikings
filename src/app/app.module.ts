bf10f24378c7b2d54c3397b3c3c73427d974990c
John Papa <papa@Johns-MacBook-Pro.local>
2020-01-13 16:23:44 -0500

    upgrading a lot of stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   imports: [BrowserModule, CoreModule, HttpClientModule, AppRoutingModule],
   declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
